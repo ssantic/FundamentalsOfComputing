a49d1d10933be0f1a36189c82a2e3fdca88cb0d4
ssantic <srdjan.santic@gmail.com>
2018-08-24 18:16:28 +0200

    Initial code

--- PrinciplesOfComputingPt1/Week1/merge.py
+++ PrinciplesOfComputingPt1/Week1/merge.py
@@ -14,11 +14,24 @@ def merge(line):
     # Iterate over the input and create an output list that has all of the
     # non-zero tiles slid over to the beginning of the list with the
     # appropriate number of zeroes at the end of the list.
+    intermediate_list = []
+
+    for element in line:
+        if element != 0:
+            intermediate_list.append(element)
+
+    while len(intermediate_list) < len(line):
+        intermediate_list.append(0)
 
     # Step 2:
     # Iterate over the list created in the previous step and create another
     # new list in which pairs of tiles in the first list are replaced with a
     # tile of twice the value and a zero tile.
+    pairs_list = []
+
+    for i in range(len(intermediate_list), 2):
+        pairs_list.append(intermediate_list[i * 2])
+        pairs_list.list_append(0)
 
     # Step 3:
     # Repeat step one using the list created in step two to slide the tiles to
