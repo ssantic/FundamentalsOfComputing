676e7f4d2da4d5f59679ffe765a221ae46320cd5
ssantic <srdjan.santic@gmail.com>
2018-08-24 15:13:33 +0200

    Add pseudocode from instructions

---
+++ PrinciplesOfComputingPt1/Week1/merge.py
@@ -0,0 +1,22 @@
+"""
+Merge function for 2048 game.
+"""
+
+
+def merge(line):
+    """
+    Function that merges a single row or column in 2048.
+    """
+    # Step 1:
+    # Iterate over the input and create an output list that has all of the
+    # non-zero tiles slid over to the beginning of the list with the
+    # appropriate number of zeroes at the end of the list.
+
+    # Step 2:
+    # Iterate over the list created in the previous step and create another
+    # new list in which pairs of tiles in the first list are replaced with a
+    # tile of twice the value and a zero tile.
+
+    # Step 3:
+    # Repeat step one using the list created in step two to slide the tiles to
+    # the beginning of the list again.
