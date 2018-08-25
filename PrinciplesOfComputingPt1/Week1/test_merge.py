9dec4c7d94851016d8ece061ee5d1963955b273e
ssantic <srdjan.santic@gmail.com>
2018-08-25 19:04:56 +0200

    Fix bug in tests

--- PrinciplesOfComputingPt1/Week1/test_merge.py
+++ PrinciplesOfComputingPt1/Week1/test_merge.py
@@ -7,13 +7,13 @@ class MergeTest(unittest.TestCase):
         self.assertEqual(merge([2, 0, 2, 4]), [4, 4, 0, 0])
 
     def test_2(self):
-        self.assertEqual(merge(0, 0, 2, 2), [4, 0, 0, 0])
+        self.assertEqual(merge([0, 0, 2, 2]), [4, 0, 0, 0])
 
     def test_3(self):
-        self.assertEqual(merge(2, 2, 0, 0), [4, 0, 0, 0])
+        self.assertEqual(merge([2, 2, 0, 0]), [4, 0, 0, 0])
 
     def test_4(self):
-        self.assertEqual(merge(2, 2, 2, 2, 2), [4, 4, 2, 0, 0])
+        self.assertEqual(merge([2, 2, 2, 2, 2]), [4, 4, 2, 0, 0])
 
     def test_5(self):
         self.assertEqual(merge([8, 16, 16, 8]), [8, 32, 8, 0])
