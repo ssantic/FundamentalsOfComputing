84a1b825f3fb7a8893884446ea0128694ebc5082
ssantic <srdjan.santic@gmail.com>
2018-09-16 13:55:06 +0200

    mc_update_scores()

--- PrinciplesOfComputingPt1/Week3/tic_tac_toe.py
+++ PrinciplesOfComputingPt1/Week3/tic_tac_toe.py
@@ -3,17 +3,42 @@ Monte Carlo Tic-Tac-Toe Player
 """
 
 import random
-import poc_ttt_gui
+#import poc_ttt_gui
 import poc_ttt_provided as provided
 
 # Constants for Monte Carlo simulator
 # You may change the values of these constants as desired, but
 #  do not change their names.
-NTRIALS = 1         # Number of trials to run
-SCORE_CURRENT = 1.0 # Score for squares played by the current player
-SCORE_OTHER = 1.0   # Score for squares played by the other player
-    
+NTRIALS = 1           # Number of trials to run
+SCORE_CURRENT = 1.0   # Score for squares played by the current player
+SCORE_OTHER = 1.0     # Score for squares played by the other player
+
+
 # Add your functions here.
+
+
+def mc_trial(board, player):
+    raise NotImplementedError
+
+
+def mc_update_scores(scores, board, player):
+    board_size = board.get_dim()[0]
+    winner = board.check_win()
+    score_player = (player == winner) * SCORE_CURRENT + (player != winner) * -SCORE_CURRENT
+    score_other = (player == winner) * -SCORE_OTHER + (player != winner) * SCORE_OTHER
+    for row in range(board_size):
+        for col in range(board_size):
+            score = (winner == board.square(row, col))
+
+
+
+def get_best_move(board, scores):
+    raise NotImplementedError
+
+
+def mc_move(board, player, trials):
+    raise NotImplementedError
+
 
 
 
