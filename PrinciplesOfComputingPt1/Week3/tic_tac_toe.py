7af32318e6174b898f5eed526eda4ac4db0f0caf
ssantic <srdjan.santic@gmail.com>
2018-09-16 17:09:43 +0200

    Fix bug in mc_trial

--- PrinciplesOfComputingPt1/Week3/tic_tac_toe.py
+++ PrinciplesOfComputingPt1/Week3/tic_tac_toe.py
@@ -19,7 +19,7 @@ SCORE_OTHER = 1.0     # Score for squares played by the other player
 
 def mc_trial(board, player):
     current_player = player
-    while board.check_win() is not None:
+    while board.check_win() is None:
         empty_squares = board.get_empty_squares()
         (row, col) = random.choice(empty_squares)
         board.move(row, col, current_player)
@@ -27,7 +27,7 @@ def mc_trial(board, player):
 
 
 def mc_update_scores(scores, board, player):
-    board_size = board.get_dim()[0]
+    board_size = board.get_dim()
     winner = board.check_win()
     score_player = (player == winner) * SCORE_CURRENT + (player != winner) * -SCORE_CURRENT
     score_other = (player == winner) * -SCORE_OTHER + (player != winner) * SCORE_OTHER
@@ -53,7 +53,7 @@ def get_best_move(board, scores):
 
 
 def mc_move(board, player, trials):
-    board_size = board.get_dim()[0]
+    board_size = board.get_dim()
     scores = [[0 for _ in range(board_size)] for _ in range(board_size)]
     for trial in range(trials):
         trial_board = board
